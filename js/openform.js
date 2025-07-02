a9f7b790af2212cf27b1696fee38a3466e34b7e0
Sherzodbek013 <fozilovsherzodbek013@gmail.com>
2025-07-02 18:28:37 +0300

    оверлов

--- js/openform.js
+++ js/openform.js
@@ -1,20 +1,23 @@
+
 document.addEventListener('DOMContentLoaded', function() {
   const openBtn = document.getElementById('open-form-btn');
   const formSection = document.getElementById('popup-form-section');
   const closeBtn = document.getElementById('close-form-btn');
 
   if (openBtn && formSection) {
-      openBtn.addEventListener('click', function() {
+    openBtn.addEventListener('click', function() {
       formSection.classList.remove('hidden');
       openBtn.classList.add('hidden');
+      document.body.style.overflow = 'hidden';
       formSection.scrollIntoView({behavior: "smooth"});
     });
   }
 
   if (closeBtn && formSection && openBtn) {
-      closeBtn.addEventListener('click', function() {
+    closeBtn.addEventListener('click', function() {
       formSection.classList.add('hidden');
       openBtn.classList.remove('hidden');
+      document.body.style.overflow = '';
     });
   }
 });
